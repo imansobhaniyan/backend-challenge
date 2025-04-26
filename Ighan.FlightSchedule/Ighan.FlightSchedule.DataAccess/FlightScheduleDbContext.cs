8c3607b52a49844c0c2dab3f9d025c9dae66657b
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2025-04-26 12:29:31 +0330

    add dbContext

---
+++ Ighan.FlightSchedule/Ighan.FlightSchedule.DataAccess/FlightScheduleDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ighan.FlightSchedule.DataAccess;
+
+public sealed class FlightScheduleDbContext : DbContext
+{
+    public FlightScheduleDbContext(DbContextOptions<FlightScheduleDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<StorageModels.Flight> Flights { get; set; }
+    public DbSet<StorageModels.Route> Routes { get; set; }
+    public DbSet<StorageModels.Subscription> Subscriptions { get; set; }
+}
