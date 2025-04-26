41e61964c14331d5632506d6ba2518c7b0742987
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2025-04-27 01:02:48 +0330

    add dbContext provider

---
+++ Ighan.FlightSchedule/Ighan.FlightSchedule.DataAccess/FlightScheduleDbContextProvider.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Ighan.FlightSchedule.DataAccess;
+
+public sealed class FlightScheduleDbContextProvider
+{
+    private static FlightScheduleDbContext? instance;
+
+    public static FlightScheduleDbContext GetInstance(IConfiguration configuration, IDataSeeder dataSeeder)
+    {
+        if (instance is null)
+        {
+            instance = getDbInstance();
+
+            instance.Database.Migrate();
+
+            if (dataSeeder.IsDataSeedNeeded(instance))
+            {
+                dataSeeder.SeedSubscriptions(getDbInstance);
+                dataSeeder.SeedRoutes(getDbInstance);
+                dataSeeder.SeedFlights(getDbInstance);
+            }
+        }
+
+        return instance;
+
+        FlightScheduleDbContext getDbInstance()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<FlightScheduleDbContext>()
+                .UseSqlServer(configuration.GetRequiredSection("ConnectionStrings")["Default"]);
+
+            return new FlightScheduleDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Ighan.FlightSchedule/Ighan.FlightSchedule.DataAccess/IDataSeeder.cs
@@ -0,0 +1,10 @@
+﻿namespace Ighan.FlightSchedule.DataAccess;
+
+public interface IDataSeeder
+{
+    bool IsDataSeedNeeded(FlightScheduleDbContext dbContext);
+
+    void SeedRoutes(Func<FlightScheduleDbContext> dbContextGenerator);
+    void SeedFlights(Func<FlightScheduleDbContext> dbContextGenerator);
+    void SeedSubscriptions(Func<FlightScheduleDbContext> dbContextGenerator);
+}
