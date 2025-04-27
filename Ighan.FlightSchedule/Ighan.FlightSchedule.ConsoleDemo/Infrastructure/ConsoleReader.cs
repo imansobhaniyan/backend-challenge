b6c9ac7ccc1f092a77ba2f40d5b6dd7939a79b3f
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2025-04-27 23:36:35 +0330

    add Console Reader

---
+++ Ighan.FlightSchedule/Ighan.FlightSchedule.ConsoleDemo/Infrastructure/ConsoleReader.cs
@@ -0,0 +1,42 @@
+﻿namespace Ighan.FlightSchedule.ConsoleDemo.Infrastructure;
+
+public sealed class ConsoleReader
+{
+    public static int? ReadInt(string message)
+    {
+        int? result = null;
+
+        do
+        {
+            Console.Write(message);
+            var value = Console.ReadLine();
+
+            if (value == "exit")
+                break;
+
+            if (int.TryParse(value, out var dateTime))
+                result = dateTime;
+        } while (result is null);
+
+        return result;
+    }
+
+    public static DateTime? ReadDateTime(string message)
+    {
+        DateTime? result = null;
+
+        do
+        {
+            Console.Write(message);
+            var value = Console.ReadLine();
+
+            if (value == "exit")
+                break;
+
+            if (DateTimeParser.TryParse(value, out var dateTime))
+                result = dateTime;
+        } while (result is null);
+
+        return result;
+    }
+}
