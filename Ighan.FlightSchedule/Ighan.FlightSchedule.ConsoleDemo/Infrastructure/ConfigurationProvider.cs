5edcd43b19fbefc942082b5a05ba87d2d48cefe4
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2025-04-27 23:41:46 +0330

    add Configuration Provider and app setting file

---
+++ Ighan.FlightSchedule/Ighan.FlightSchedule.ConsoleDemo/Infrastructure/ConfigurationProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Ighan.FlightSchedule.ConsoleDemo.Infrastructure;
+
+public sealed class ConfigurationProvider
+{
+    private static IConfiguration? instance;
+
+    public static IConfiguration GetInstance()
+    {
+        if (instance is null)
+        {
+            instance = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        return instance;
+    }
+}
