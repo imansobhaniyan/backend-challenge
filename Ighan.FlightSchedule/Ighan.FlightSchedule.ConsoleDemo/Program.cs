b86c1369ac814ace642cc847c512491e1464126e
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2025-04-28 00:47:02 +0330

    use time logger program

--- Ighan.FlightSchedule/Ighan.FlightSchedule.ConsoleDemo/Program.cs
+++ Ighan.FlightSchedule/Ighan.FlightSchedule.ConsoleDemo/Program.cs
@@ -2,6 +2,8 @@
 using Ighan.FlightSchedule.ConsoleDemo.Services;
 using Ighan.FlightSchedule.DataAccess;
 
+using System.Diagnostics;
+
 var commands = new CommandLineParser(args);
 
 var startDate = commands.GetDateTimeAt(0) ?? ConsoleReader.ReadDateTime($"Please enter start date ({DateTimeParser.DateFormat}) or 'exit': ");
@@ -19,10 +21,48 @@ var agencyId = commands.GetInt(2) ?? ConsoleReader.ReadInt("Please enter agency 
 if (agencyId is null)
     return;
 
+var serviceLogger = TimeLogger.Start("Program");
+
 using var db = FlightScheduleDbContextProvider.GetInstance(ConfigurationProvider.GetInstance(), new DataSeeder());
+
+serviceLogger.LogDurationAndReset("db context inited in:");
 
 var service = new ReportService(db);
 
 await service.GenerateReportAsync(startDate.Value, endDate.Value, agencyId.Value);
 
-Console.Write("Press any key to exit"); Console.ReadLine();
+serviceLogger.LogDuration("Report generated in:");
+serviceLogger.Finish();
+
+Console.WriteLine();
+Console.WriteLine("Choose what to do next: ");
+Console.WriteLine("1. Open file with notepad");
+Console.WriteLine("2. Open file directory");
+Console.WriteLine("3. Exit");
+
+var input = ConsoleReader.ReadInt("Enter the option: ");
+
+if (input is null)
+    return;
+
+switch (input)
+{
+    case 1:
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = "notepad.exe",
+            Arguments = Path.Combine("Results", "results.csv"),
+            UseShellExecute = true
+        });
+        break;
+    case 2:
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = "explorer.exe",
+            Arguments = Path.Combine(Directory.GetCurrentDirectory(), "Results"),
+            UseShellExecute = true
+        });
+        break;
+    default:
+        return;
+}
