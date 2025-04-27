37157e3ab476d2daa0f750f7b11af6015b400b6b
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2025-04-27 23:39:16 +0330

    add Date Time Parser

---
+++ Ighan.FlightSchedule/Ighan.FlightSchedule.ConsoleDemo/Infrastructure/DateTimeParser.cs
@@ -0,0 +1,11 @@
+﻿namespace Ighan.FlightSchedule.ConsoleDemo.Infrastructure;
+
+public static class DateTimeParser
+{
+    public const string DateFormat = "yyyy-MM-dd";
+
+    public static bool TryParse(string? stringValue, out DateTime dateTime)
+    {
+        return DateTime.TryParseExact(stringValue, DateFormat, null, System.Globalization.DateTimeStyles.None, out dateTime);
+    }
+}
