72d3f0cbe5dbb608d866309ceac97bbc48493557
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2025-04-27 23:36:12 +0330

    add Command Line Parser

---
+++ Ighan.FlightSchedule/Ighan.FlightSchedule.ConsoleDemo/Infrastructure/CommandLineParser.cs
@@ -0,0 +1,27 @@
+﻿namespace Ighan.FlightSchedule.ConsoleDemo.Infrastructure;
+
+public sealed class CommandLineParser
+{
+    private readonly string[] _args;
+
+    public CommandLineParser(string[] args)
+    {
+        _args = args;
+    }
+
+    public int? GetInt(int index)
+    {
+        if (_args.Length > index && int.TryParse(_args[index], out var value))
+            return value;
+
+        return null;
+    }
+
+    public DateTime? GetDateTimeAt(int index)
+    {
+        if (_args.Length > index && DateTimeParser.TryParse(_args[index], out var value))
+            return value;
+
+        return null;
+    }
+}
