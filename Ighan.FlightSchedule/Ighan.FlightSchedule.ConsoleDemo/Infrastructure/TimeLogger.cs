7cd28d6d6a9df3175e5845a11209b711bd59794a
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2025-04-28 00:45:52 +0330

    add time logger

---
+++ Ighan.FlightSchedule/Ighan.FlightSchedule.ConsoleDemo/Infrastructure/TimeLogger.cs
@@ -0,0 +1,44 @@
+﻿namespace Ighan.FlightSchedule.ConsoleDemo.Infrastructure;
+
+public class TimeLogger
+{
+    private readonly DateTime _createTime;
+    private DateTime _startTime;
+    private readonly string? _messagePrefix;
+
+    private TimeLogger(string? messagePrefix)
+    {
+        _createTime = _startTime = DateTime.Now;
+        _messagePrefix = messagePrefix;
+
+        Log("Started");
+    }
+
+    public void LogDuration(string message)
+    {
+        Log($"{message} {DateTime.Now - _startTime:ss\\.fff}");
+    }
+
+    public void LogDurationAndReset(string message)
+    {
+        LogDuration(message);
+
+        _startTime = DateTime.Now;
+    }
+
+    public void Finish()
+    {
+        _startTime = _createTime;
+        LogDuration("Finished in:");
+    }
+
+    private void Log(string message)
+    {
+        Console.WriteLine($"{_messagePrefix} - {message}".Trim(' ', '-'));
+    }
+
+    public static TimeLogger Start(string? messagePrefix)
+    {
+        return new TimeLogger(messagePrefix);
+    }
+}
